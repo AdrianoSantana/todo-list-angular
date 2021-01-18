e360b3ab098db45e48832d0e3fa0a9529c50768c
ans3 <ans3@cin.ufpe.br>
2021-01-18 08:19:35 -0300

    Todo list concluida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,83 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public mode: string = "list";
+  public notification: string = "";
+  public todos: Todo[] = [];
+  public title: string = "Minhas Tarefas";
+  public form: FormGroup;
+  public idAtual: number = 0;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+
+    this.load();
+  }
+
+  showNotification() {
+    this.notification = "Tarefa adicionada com sucesso!";
+    setTimeout(() => {
+      this.notification = "";
+    }, 1500)
+    
+  }
+
+  add() {
+    this.idAtual += 1;
+    const title = this.form.controls['title'].value;
+    this.todos.push(new Todo(title, false, this.idAtual));
+    this.save();
+    this.showNotification();
+  }
+
+  clear() {
+    this.form.reset()
+  }
+
+  remove(tarefa: Todo) {
+    const index = this.todos.indexOf(tarefa);
+    if (index !== -1) {
+      this.todos.splice(index, 1); // indice, quantidade de itens que queremos deletar
+    }
+    this.save();
+  }
+
+  markAsDone(tarefa: Todo) {
+    tarefa.done = true;
+    this.save();
+  }
+
+  markAsUndone(tarefa: Todo) {
+    tarefa.done = false;
+    this.save();
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    if (data) {
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
+    }
+  }
+
+  changeMode(newMode: string){
+    this.mode = newMode;
+    if (this.mode == 'add') {
+      this.title = "Nova Tarefa";
+    } else {
+      this.title = "Minhas Tarefas"
+    }
+  }
 }
